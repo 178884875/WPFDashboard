8ff22bb8b6dd606bbf33bed9398e4c10cdfa18d0
178884875 <178884875@qq.com>
2022-06-05 16:01:24 +0800

    Init

---
+++ WpfDashBoard/UserControls/Country.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfDashBoard.UserControls
+{
+    /// <summary>
+    /// Country.xaml 的交互逻辑
+    /// </summary>
+    public partial class Country : UserControl
+    {
+        public Country()
+        {
+            InitializeComponent();
+        }
+
+
+        public string CountryName
+        {
+            get { return (string)GetValue(CountryNameProperty); }
+            set { SetValue(CountryNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CountryName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CountryNameProperty =
+            DependencyProperty.Register("CountryName", typeof(string), typeof(Country));
+
+
+        public string Price
+        {
+            get { return (string)GetValue(PriceProperty); }
+            set { SetValue(PriceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Price.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PriceProperty =
+            DependencyProperty.Register("Price", typeof(string), typeof(Country));
+
+
+        public bool IsLevelUp
+        {
+            get { return (bool)GetValue(IsLevelUpProperty); }
+            set { SetValue(IsLevelUpProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsLevelUpProperty =
+            DependencyProperty.Register("IsLevelUp", typeof(bool), typeof(Country));
+
+        public ImageSource Flag
+        {
+            get { return (ImageSource)GetValue(FlagProperty); }
+            set { SetValue(FlagProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Flag.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FlagProperty =
+            DependencyProperty.Register("Flag", typeof(ImageSource), typeof(Country));
+
+
+
+
+    }
+}
---
+++ WpfDashBoard/UserControls/InfoCrad.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDashBoard.UserControls
+{
+    /// <summary>
+    /// Interaction logic for InfoCrad.xaml
+    /// </summary>
+    public partial class InfoCrad : UserControl
+    {
+        public InfoCrad()
+        {
+            InitializeComponent();
+        }
+
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(InfoCrad));
+
+
+
+        public string Number
+        {
+            get { return (string)GetValue(NumberProperty); }
+            set { SetValue(NumberProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Number.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NumberProperty =
+            DependencyProperty.Register("Number", typeof(string), typeof(InfoCrad));
+
+
+
+
+
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsActiveProperty =
+            DependencyProperty.Register("IsActive", typeof(bool), typeof(InfoCrad));
+
+
+
+
+        public bool IsTechnical
+        {
+            get { return (bool)GetValue(IsTechnicalProperty); }
+            set { SetValue(IsTechnicalProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsTechnical.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsTechnicalProperty =
+            DependencyProperty.Register("IsTechnical", typeof(bool), typeof(InfoCrad));
+
+
+
+    }
+}
